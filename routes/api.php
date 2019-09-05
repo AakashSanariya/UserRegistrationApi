cd78e05d01058d2cf98902cde741518dc4456b60
Akash_Sanariya <akash.sanariya@brainvire.com>
2019-09-05 17:09:37 +0530

    Small Bug Fix

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 Route::post('user/login', 'UserRegistrationController@login');
+Route::post('user/register', 'UserRegistrationController@store');
 Route::group(['middleware' => 'auth:api'], function (){
     Route::resource('user', 'UserRegistrationController');
     Route::post('user/update/{id}', 'UserRegistrationController@updateDetails');
